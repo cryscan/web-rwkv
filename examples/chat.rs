ce83a163510cab769f9cdf53026760b5115d006b
cedruszhang <cedruszhang@tencent.com>
2023-07-12 19:16:08 +0800

    Chat sampler.

--- examples/chat.rs
+++ examples/chat.rs
@@ -1,3 +1,4 @@
+use ahash::{HashMap, HashMapExt};
 use anyhow::Result;
 use clap::Parser;
 use itertools::Itertools;
@@ -9,33 +10,48 @@ use std::{
 };
 use web_rwkv::{Environment, Model, Tokenizer};
 
-fn softmax(data: &[f32]) -> Vec<f32> {
-    let exp = data.iter().copied().map(f32::exp).collect_vec();
-    let sum: f32 = exp.iter().sum();
-    exp.into_iter().map(|x| x / sum).collect_vec()
+struct Sampler {
+    pub top_p: f32,
+    pub temp: f32,
+    pub presence_penalty: f32,
+    pub frequency_penalty: f32,
 }
 
-fn sample(probs: &[f32], top_p: f32) -> u16 {
-    let sorted = probs
-        .iter()
-        .copied()
-        .enumerate()
-        .sorted_by(|a, b| a.1.total_cmp(&b.1).reverse())
-        .scan((0usize, 0.0f32), |state, x| {
-            if state.1 > top_p {
-                return None;
-            }
-            Some((x.0, state.1 + x.1))
-        })
-        .collect_vec();
+impl Sampler {
+    pub fn sample(&self, mut logits: Vec<f32>, occurrences: &HashMap<u16, usize>) -> u16 {
+        for (&token, &count) in occurrences.iter() {
+            let presence_penalty = match count > 0 {
+                true => self.presence_penalty,
+                false => 0.0,
+            };
+            logits[token as usize] -= presence_penalty + count as f32 * self.frequency_penalty;
+        }
 
-    let rand = fastrand::f32() * sorted.last().unwrap().1;
-    let (token, _) = sorted
-        .iter()
-        .find_or_first(|&&(_, cum)| rand <= cum)
-        .unwrap();
+        let probs = softmax(logits);
+        let sorted: Vec<_> = probs
+            .into_iter()
+            .enumerate()
+            .sorted_by(|(_, x), (_, y)| x.total_cmp(&y).reverse())
+            .scan((0, 0.0, 0.0), |(_, acc, _), (id, x)| {
+                (*acc <= self.top_p).then(|| (id, *acc + x, x))
+            })
+            .map(|(id, _, p)| (id, p.powf(1.0 / self.temp)))
+            .scan((0, 0.0), |(_, acc), (id, x)| Some((id, *acc + x)))
+            .collect();
 
-    *token as u16
+        let rand = fastrand::f32() * sorted.last().unwrap().1;
+        let (token, _) = sorted
+            .iter()
+            .find_or_first(|&&(_, cum)| rand <= cum)
+            .unwrap();
+        *token as u16
+    }
+}
+
+fn softmax(data: Vec<f32>) -> Vec<f32> {
+    let exp: Vec<_> = data.into_iter().map(f32::exp).collect();
+    let sum: f32 = exp.iter().sum();
+    exp.into_iter().map(|x| x / sum).collect()
 }
 
 async fn create_environment() -> Result<Environment> {
@@ -60,11 +76,21 @@ async fn load_model(env: &Environment, model: PathBuf) -> Result<Model> {
     Ok(model)
 }
 
-async fn run(model: PathBuf) -> Result<()> {
+async fn run(args: Args) -> Result<()> {
     let env = create_environment().await?;
-
     let tokenizer = load_tokenizer().await?;
+
+    let model = args
+        .model
+        .unwrap_or("assets/models/RWKV-4-World-0.4B-v1-20230529-ctx4096.st".into());
     let model = load_model(&env, model).await?;
+
+    let sampler = Sampler {
+        top_p: args.top_p,
+        temp: args.temp,
+        presence_penalty: args.presence_penalty,
+        frequency_penalty: args.frequency_penalty,
+    };
 
     let user = "User";
     let bot = "Assistant";
@@ -81,6 +107,7 @@ async fn run(model: PathBuf) -> Result<()> {
     loop {
         let mut model_text = String::new();
         let mut user_text = String::new();
+        let mut occurrences = HashMap::new();
 
         print!("{}: ", user);
         std::io::stdout().flush()?;
@@ -104,9 +131,7 @@ async fn run(model: PathBuf) -> Result<()> {
             let mut logits = model.run(&tokens, &state)?;
             logits[0] = f32::NEG_INFINITY;
 
-            let probs = softmax(&logits);
-
-            let token = sample(&probs, 0.5);
+            let token = sampler.sample(logits, &occurrences);
             let word = String::from_utf8(tokenizer.decode(&[token])?)?;
 
             model_text += &word;
@@ -114,6 +139,11 @@ async fn run(model: PathBuf) -> Result<()> {
             std::io::stdout().flush()?;
 
             tokens = vec![token];
+            if !occurrences.contains_key(&token) {
+                occurrences.insert(token, 1);
+            } else {
+                *occurrences.get_mut(&token).unwrap() += 1;
+            }
         }
     }
 
@@ -125,13 +155,17 @@ async fn run(model: PathBuf) -> Result<()> {
 struct Args {
     #[arg(short, long, value_name = "FILE")]
     model: Option<PathBuf>,
+    #[arg(long, default_value_t = 0.5)]
+    top_p: f32,
+    #[arg(long, default_value_t = 1.0)]
+    temp: f32,
+    #[arg(long, default_value_t = 0.3)]
+    presence_penalty: f32,
+    #[arg(long, default_value_t = 0.3)]
+    frequency_penalty: f32,
 }
 
 fn main() {
     let args = Args::parse();
-    let model = args
-        .model
-        .unwrap_or("assets/models/RWKV-4-World-0.4B-v1-20230529-ctx4096.st".into());
-
-    pollster::block_on(run(model)).unwrap();
+    pollster::block_on(run(args)).unwrap();
 }
