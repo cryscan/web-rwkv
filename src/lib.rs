1e45c57f2753c8ae9eedaa61dd38291ef00c482d
cryscan <cryscan@umich.edu>
2023-07-20 00:06:42 +0800

    Default to high power performance.

--- src/lib.rs
+++ src/lib.rs
@@ -5,4 +5,3 @@ mod tokenizer;
 pub use environment::{CreateEnvironmentError, Environment};
 pub use model::{BackedModelState, Model, ModelBuffer, ModelState};
 pub use tokenizer::{Tokenizer, TokenizerError};
-pub use wgpu::PowerPreference;
