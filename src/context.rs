1996d91bbf22a8a220b8466011dfafe0ca530b0b
cryscan <lepetitsquarth@sjtu.edu.cn>
2023-08-16 00:07:30 +0800

    Add default pipelines.

--- src/context.rs
+++ src/context.rs
@@ -7,8 +7,9 @@ use std::{
 };
 use uid::Id;
 use wgpu::{
-    Adapter, Backends, ComputePipeline, ComputePipelineDescriptor, Device, DeviceDescriptor,
-    Dx12Compiler, InstanceDescriptor, PipelineLayout, Queue, ShaderModuleDescriptor,
+    Adapter, Backends, BindGroupLayoutDescriptor, BindGroupLayoutEntry, ComputePipeline,
+    ComputePipelineDescriptor, Device, DeviceDescriptor, Dx12Compiler, InstanceDescriptor,
+    PipelineLayout, PipelineLayoutDescriptor, Queue, ShaderModuleDescriptor, ShaderStages,
 };
 
 #[derive(Clone)]
@@ -110,13 +111,7 @@ impl Context {
         })
     }
 
-    pub fn add_pipeline(
-        &mut self,
-        name: &str,
-        shader: &str,
-        entry_point: &str,
-        layout: Option<&PipelineLayout>,
-    ) {
+    pub fn add_pipeline(&mut self, name: &str, shader: &str, layout: Option<&PipelineLayout>) {
         let module = &self.device.create_shader_module(ShaderModuleDescriptor {
             label: Some(name),
             source: wgpu::ShaderSource::Wgsl(Cow::from(shader)),
@@ -127,25 +122,144 @@ impl Context {
                 label: Some(name),
                 layout,
                 module,
-                entry_point,
+                entry_point: name,
             });
 
         let mut pipelines = self.pipelines.write().unwrap();
-        pipelines.insert(
-            String::from_str(name).expect("ill-formed pipeline name"),
-            pipeline,
-        );
+        pipelines.insert(String::from_str(name).expect("Bad pipeline name"), pipeline);
     }
 
     pub fn with_pipeline(
         mut self,
         name: &str,
         shader: &str,
-        entry_point: &str,
         layout: Option<&PipelineLayout>,
     ) -> Self {
-        self.add_pipeline(name, shader, entry_point, layout);
+        self.add_pipeline(name, shader, layout);
         self
+    }
+
+    pub fn with_default_pipelines(self) -> Self {
+        self.with_pipeline("layer_norm", include_str!("shaders/layer_norm.wgsl"), None)
+            .with_pipeline("matmul", include_str!("shaders/matmul.wgsl"), None)
+            .with_pipeline(
+                "matmul_int8",
+                include_str!("shaders/matmul_int8.wgsl"),
+                None,
+            )
+            .with_pipeline(
+                "token_shift",
+                include_str!("shaders/token_shift.wgsl"),
+                None,
+            )
+            .with_pipeline("token_mix", include_str!("shaders/token_mix.wgsl"), None)
+            .with_pipeline("add", include_str!("shaders/add.wgsl"), None)
+            .with_pipeline(
+                "squared_relu",
+                include_str!("shaders/squared_relu.wgsl"),
+                None,
+            )
+            .with_pipeline(
+                "channel_mix",
+                include_str!("shaders/channel_mix.wgsl"),
+                None,
+            )
+            .with_pipeline("softmax", include_str!("shaders/softmax.wgsl"), None)
+    }
+
+    pub fn with_quantize_pipelines(self) -> Self {
+        let shader = include_str!("shaders/quantize_int8.wgsl");
+        let bind_group_layout = self
+            .device
+            .create_bind_group_layout(&BindGroupLayoutDescriptor {
+                label: None,
+                entries: &[
+                    BindGroupLayoutEntry {
+                        binding: 0,
+                        visibility: ShaderStages::COMPUTE,
+                        ty: wgpu::BindingType::Buffer {
+                            ty: wgpu::BufferBindingType::Uniform,
+                            has_dynamic_offset: false,
+                            min_binding_size: None,
+                        },
+                        count: None,
+                    },
+                    BindGroupLayoutEntry {
+                        binding: 1,
+                        visibility: ShaderStages::COMPUTE,
+                        ty: wgpu::BindingType::Buffer {
+                            ty: wgpu::BufferBindingType::Storage { read_only: false },
+                            has_dynamic_offset: false,
+                            min_binding_size: None,
+                        },
+                        count: None,
+                    },
+                    BindGroupLayoutEntry {
+                        binding: 2,
+                        visibility: ShaderStages::COMPUTE,
+                        ty: wgpu::BindingType::Buffer {
+                            ty: wgpu::BufferBindingType::Storage { read_only: false },
+                            has_dynamic_offset: false,
+                            min_binding_size: None,
+                        },
+                        count: None,
+                    },
+                    BindGroupLayoutEntry {
+                        binding: 3,
+                        visibility: ShaderStages::COMPUTE,
+                        ty: wgpu::BindingType::Buffer {
+                            ty: wgpu::BufferBindingType::Storage { read_only: false },
+                            has_dynamic_offset: false,
+                            min_binding_size: None,
+                        },
+                        count: None,
+                    },
+                    BindGroupLayoutEntry {
+                        binding: 4,
+                        visibility: ShaderStages::COMPUTE,
+                        ty: wgpu::BindingType::Buffer {
+                            ty: wgpu::BufferBindingType::Storage { read_only: false },
+                            has_dynamic_offset: false,
+                            min_binding_size: None,
+                        },
+                        count: None,
+                    },
+                    BindGroupLayoutEntry {
+                        binding: 5,
+                        visibility: ShaderStages::COMPUTE,
+                        ty: wgpu::BindingType::Buffer {
+                            ty: wgpu::BufferBindingType::Storage { read_only: false },
+                            has_dynamic_offset: false,
+                            min_binding_size: None,
+                        },
+                        count: None,
+                    },
+                    BindGroupLayoutEntry {
+                        binding: 6,
+                        visibility: ShaderStages::COMPUTE,
+                        ty: wgpu::BindingType::Buffer {
+                            ty: wgpu::BufferBindingType::Storage { read_only: false },
+                            has_dynamic_offset: false,
+                            min_binding_size: None,
+                        },
+                        count: None,
+                    },
+                ],
+            });
+        let layout = self
+            .device
+            .create_pipeline_layout(&PipelineLayoutDescriptor {
+                label: None,
+                bind_group_layouts: &[&bind_group_layout],
+                push_constant_ranges: &[],
+            });
+        let layout = Some(&layout);
+
+        self.with_pipeline("quantize", shader, layout)
+            .with_pipeline("compute_mx", shader, layout)
+            .with_pipeline("compute_my", shader, layout)
+            .with_pipeline("compute_rx", shader, layout)
+            .with_pipeline("compute_ry", shader, layout)
     }
 }
 
