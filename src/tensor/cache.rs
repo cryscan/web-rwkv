9caf708926559544d8396409d3131874ff6ad74d
cryscan <lepetitsquarth@sjtu.edu.cn>
2023-08-28 00:32:39 +0800

    Remove `RefCell`.

--- src/tensor/cache.rs
+++ src/tensor/cache.rs
@@ -1,5 +1,4 @@
 use std::{
-    cell::RefCell,
     collections::HashMap,
     hash::Hash,
     sync::{Arc, RwLock},
@@ -9,7 +8,7 @@ use std::{
 #[derive(Debug)]
 pub struct ResourceCache<K, V> {
     max_count: usize,
-    map: RefCell<RwLock<HashMap<K, (Arc<V>, usize)>>>,
+    map: RwLock<HashMap<K, (Arc<V>, usize)>>,
 }
 
 impl<K, V> Default for ResourceCache<K, V> {
@@ -26,16 +25,14 @@ where
     K: PartialEq + Eq + Hash,
 {
     pub fn query(&self, key: &K) -> Option<Arc<V>> {
-        let map = self.map.borrow();
-        let map = map.read().unwrap();
+        let map = self.map.read().unwrap();
         map.get(key).cloned().map(|(v, _)| v)
     }
 
     pub fn request(&self, key: K, op: impl FnOnce() -> V) -> Arc<V> {
         let buffer = self.query(&key).unwrap_or_else(|| Arc::new(op()));
 
-        let map = self.map.borrow_mut();
-        let mut map = map.write().unwrap();
+        let mut map = self.map.write().unwrap();
         map.insert(key, (buffer.clone(), 0));
 
         map.retain(|_, (_, count)| {
