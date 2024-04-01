80272fd82f73b4bf6badec1354f1318077c17a63
cryscan <cryscan@umich.edu>
2024-04-01 13:22:15 +0800

    Checkpoint x2.

--- src/model/pipeline.rs
+++ src/model/pipeline.rs
@@ -7,7 +7,6 @@ use wgpu::CommandBuffer;
 
 use super::OutputType;
 use crate::{
-    context::Context,
     num::Float,
     tensor::{kind::ReadWrite, TensorCpu, TensorError, TensorGpu},
 };
@@ -17,80 +16,65 @@ type BoxedFuture<Output> = Pin<Box<dyn Future<Output = Output>>>;
 
 pub trait Pipeline {
     type Output;
-    type Backed;
     type Error;
 
+    /// Wait for required data to be ready and submit the GPU jobs.
     fn execute(self) -> impl Future<Output = Result<Self::Output, Self::Error>>;
-    fn back(output: Self::Output) -> impl Future<Output = Self::Backed>;
 }
 
-pub trait ModelInput: IntoFuture<Output = Vec<u16>, IntoFuture = BoxedFuture<Vec<u16>>> {
+pub trait Prompt: IntoFuture<Output = Vec<u16>, IntoFuture = BoxedFuture<Vec<u16>>> {
+    /// Number of tokens in the prompt.
     fn num_token(&self) -> usize;
+
+    /// Output type of the prompt, can either only compute the last token's prediction or compute all tokens'.
     fn output_type(&self) -> OutputType;
-    fn split_at(self, mid: usize) -> (Box<dyn ModelInput>, Box<dyn ModelInput>);
+
+    /// Split the prompt into two at `mid`. `mid` must be less than `num_token`.
+    fn split_at(self, mid: usize) -> (Box<dyn Prompt>, Box<dyn Prompt>);
 }
 
 #[derive(Debug, Default, Clone)]
-pub struct ModelInputLastOutput(pub Vec<u16>);
+pub struct PresentPrompt {
+    pub tokens: Vec<u16>,
+    pub output_type: OutputType,
+}
 
-impl IntoFuture for ModelInputLastOutput {
+impl IntoFuture for PresentPrompt {
     type Output = Vec<u16>;
     type IntoFuture = BoxedFuture<Vec<u16>>;
 
     fn into_future(self) -> Self::IntoFuture {
-        Box::pin(async move { self.0 })
+        Box::pin(async move { self.tokens })
     }
 }
 
-impl ModelInput for ModelInputLastOutput {
+impl Prompt for PresentPrompt {
     fn num_token(&self) -> usize {
-        self.0.len()
+        self.tokens.len()
     }
 
     fn output_type(&self) -> OutputType {
-        OutputType::Last
+        self.output_type
     }
 
-    fn split_at(self, mid: usize) -> (Box<dyn ModelInput>, Box<dyn ModelInput>) {
-        let (head, tail) = self.0.split_at(mid);
-        (Box::new(Self(head.to_vec())), Box::new(Self(tail.to_vec())))
-    }
-}
-
-pub struct ModelInputFullOutput(pub Vec<u16>);
-
-impl IntoFuture for ModelInputFullOutput {
-    type Output = Vec<u16>;
-    type IntoFuture = BoxedFuture<Vec<u16>>;
-
-    fn into_future(self) -> Self::IntoFuture {
-        Box::pin(async move { self.0 })
+    fn split_at(self, mid: usize) -> (Box<dyn Prompt>, Box<dyn Prompt>) {
+        let output_type = self.output_type;
+        let (head, tail) = self.tokens.split_at(mid);
+        let head = Self {
+            tokens: head.to_vec(),
+            output_type,
+        };
+        let tail = Self {
+            tokens: tail.to_vec(),
+            output_type,
+        };
+        (Box::new(head), Box::new(tail))
     }
 }
 
-impl ModelInput for ModelInputFullOutput {
-    fn num_token(&self) -> usize {
-        self.0.len()
-    }
+pub struct FuturePrompt<F: Future<Output = u16> + 'static>(pub F);
 
-    fn output_type(&self) -> OutputType {
-        OutputType::Full
-    }
-
-    fn split_at(self, mid: usize) -> (Box<dyn ModelInput>, Box<dyn ModelInput>) {
-        let (head, tail) = self.0.split_at(mid);
-        (Box::new(Self(head.to_vec())), Box::new(Self(tail.to_vec())))
-    }
-}
-
-pub struct ModelInputFutureToken<F>(pub F)
-where
-    F: Future<Output = u16> + 'static;
-
-impl<F> IntoFuture for ModelInputFutureToken<F>
-where
-    F: Future<Output = u16> + 'static,
-{
+impl<F: Future<Output = u16> + 'static> IntoFuture for FuturePrompt<F> {
     type Output = Vec<u16>;
     type IntoFuture = BoxedFuture<Vec<u16>>;
 
@@ -99,10 +83,7 @@ where
     }
 }
 
-impl<F> ModelInput for ModelInputFutureToken<F>
-where
-    F: Future<Output = u16> + 'static,
-{
+impl<F: Future<Output = u16> + 'static> Prompt for FuturePrompt<F> {
     fn num_token(&self) -> usize {
         1
     }
@@ -111,41 +92,35 @@ where
         OutputType::Last
     }
 
-    fn split_at(self, mid: usize) -> (Box<dyn ModelInput>, Box<dyn ModelInput>) {
+    fn split_at(self, mid: usize) -> (Box<dyn Prompt>, Box<dyn Prompt>) {
         match mid {
-            0 => (Box::<ModelInputLastOutput>::default(), Box::new(self)),
-            1 => (Box::new(self), Box::<ModelInputLastOutput>::default()),
+            0 => (Box::<PresentPrompt>::default(), Box::new(self)),
+            1 => (Box::new(self), Box::<PresentPrompt>::default()),
             _ => panic!("split at {mid} out of range"),
         }
     }
 }
 
 pub struct Run<F: Float> {
-    context: Context,
     command: CommandBuffer,
-
     future: BoxedFuture<TensorCpu<'static, F>>,
     input: TensorGpu<F, ReadWrite>,
-    output: TensorGpu<F, ReadWrite>,
+    output: Batch<TensorGpu<F, ReadWrite>>,
 }
 
 impl<F: Float> Pipeline for Run<F> {
-    type Output = TensorGpu<F, ReadWrite>;
-    type Backed = TensorCpu<'static, F>;
+    type Output = Batch<TensorGpu<F, ReadWrite>>;
     type Error = TensorError;
 
     async fn execute(self) -> Result<Self::Output, Self::Error> {
+        let context = self.input.context();
         let tensor = self.future.await;
         self.input.load(&tensor)?;
-        self.context.queue.submit(Some(self.command));
+        context.queue.submit(Some(self.command));
         Ok(self.output)
-    }
-
-    async fn back(output: Self::Output) -> Self::Backed {
-        output.back_async().await
     }
 }
 
 pub trait ModelRun<F: Float, M, S> {
-    fn run(model: &M, state: &S, tokens: &mut Batch<Box<dyn ModelInput>>) -> Run<F>;
+    fn run(model: &M, state: &S, tokens: &mut Batch<Box<dyn Prompt>>) -> Run<F>;
 }
