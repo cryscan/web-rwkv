2f628de019c9e0d481bd08050b8c1867ae1d88ac
cryscan <cryscan@umich.edu>
2024-04-03 01:42:34 +0800

    Start working on the tokio runtime.

--- src/runtime/tokio.rs
+++ src/runtime/tokio.rs
@@ -1,1 +1,15 @@
+use super::{Run, Softmax};
 
+pub struct Runtime<Model, State> {
+    model: Model,
+    state: State,
+}
+
+impl<Model, State> Runtime<Model, State>
+where
+    Runtime<Model, State>: Run + Softmax,
+{
+    pub fn new(model: Model, state: State) -> Self {
+        Self { model, state }
+    }
+}
