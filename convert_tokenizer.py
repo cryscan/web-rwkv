0b1e093fe5080eabe342231172de3b2ffd2193f6
cryscan <cryscan@umich.edu>
2024-06-15 17:56:37 +0800

    Add binary-only vocab.

--- convert_tokenizer.py
+++ convert_tokenizer.py
@@ -2,8 +2,25 @@
 
 import json
 
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--input",
+    type=str,
+    default="rwkv_vocab_v20230424.txt",
+    help="Path to input txt")
+parser.add_argument(
+    "--output",
+    type=str,
+    default="rwkv_vocab_v20230424.json",
+    help="Path to output JSON",
+)
+args = parser.parse_args()
+
+
 I_TO_TOKEN = {}
-lines = open("rwkv_vocab_v20230424.txt", "r", encoding="utf-8").readlines()
+lines = open(args.input, "r", encoding="utf-8").readlines()
 for l in lines:
     idx = int(l[:l.index(' ')])
     x = eval(l[l.index(' '):l.rindex(' ')])
@@ -11,5 +28,5 @@ for l in lines:
         x = list(x)
     I_TO_TOKEN[idx] = x
 
-out = open("rwkv_vocab_v20230424.json", "w")
+out = open(args.output, "w")
 out.write(json.dumps(I_TO_TOKEN, indent=4))
