6bc9547f1fbfa8a22290f406b3b34dd697e24d4b
cryscan <cryscan@umich.edu>
2025-02-27 20:09:16 +0800

    Clippy.

--- crates/web-rwkv-derive/src/serde/de.rs
+++ crates/web-rwkv-derive/src/serde/de.rs
@@ -213,7 +213,7 @@ fn needs_deserialize_bound(field: &attr::Field, variant: Option<&attr::Variant>)
     !field.skip_deserializing()
         && field.deserialize_with().is_none()
         && field.de_bound().is_none()
-        && variant.map_or(true, |variant| {
+        && variant.is_none_or(|variant| {
             !variant.skip_deserializing()
                 && variant.deserialize_with().is_none()
                 && variant.de_bound().is_none()
