8904f1c9380a537a0ac908ffdffa56b218d2e691
cryscan <cryscan@umich.edu>
2025-10-20 21:26:35 +0800

    Add `deserialize_in_place` feature for `web-rwkv-derive`.

--- crates/web-rwkv-derive/src/serde/mod.rs
+++ crates/web-rwkv-derive/src/serde/mod.rs
@@ -1,3 +1,56 @@
+//! This crate provides Serde's derive macro for `DeserializeSeed`.
+
+#![cfg_attr(not(check_cfg), allow(unexpected_cfgs))]
+// Ignored clippy lints
+#![allow(
+    // clippy false positive: https://github.com/rust-lang/rust-clippy/issues/7054
+    clippy::branches_sharing_code,
+    clippy::cognitive_complexity,
+    // clippy bug: https://github.com/rust-lang/rust-clippy/issues/7575
+    clippy::collapsible_match,
+    clippy::derive_partial_eq_without_eq,
+    clippy::enum_variant_names,
+    // clippy bug: https://github.com/rust-lang/rust-clippy/issues/6797
+    clippy::manual_map,
+    clippy::match_like_matches_macro,
+    clippy::needless_lifetimes,
+    clippy::needless_pass_by_value,
+    clippy::too_many_arguments,
+    clippy::trivially_copy_pass_by_ref,
+    clippy::used_underscore_binding,
+    clippy::wildcard_in_or_patterns,
+    // clippy bug: https://github.com/rust-lang/rust-clippy/issues/5704
+    clippy::unnested_or_patterns,
+)]
+// Ignored clippy_pedantic lints
+#![allow(
+    clippy::cast_possible_truncation,
+    clippy::checked_conversions,
+    clippy::doc_markdown,
+    clippy::elidable_lifetime_names,
+    clippy::enum_glob_use,
+    clippy::indexing_slicing,
+    clippy::items_after_statements,
+    clippy::let_underscore_untyped,
+    clippy::manual_assert,
+    clippy::map_err_ignore,
+    clippy::match_same_arms,
+    // clippy bug: https://github.com/rust-lang/rust-clippy/issues/6984
+    clippy::match_wildcard_for_single_variants,
+    clippy::module_name_repetitions,
+    clippy::must_use_candidate,
+    clippy::similar_names,
+    clippy::single_match_else,
+    clippy::struct_excessive_bools,
+    clippy::too_many_lines,
+    clippy::uninlined_format_args,
+    clippy::unseparated_literal_suffix,
+    clippy::unused_self,
+    clippy::use_self,
+    clippy::wildcard_imports
+)]
+#![cfg_attr(all(test, exhaustive), feature(non_exhaustive_omitted_patterns_lint))]
+#![allow(unknown_lints, mismatched_lifetime_syntaxes)]
 #![allow(dead_code)]
 
 extern crate proc_macro2;
